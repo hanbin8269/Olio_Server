8edc4d14d1e7fb981b5ec79499724b0c6885344e
hmoon630 <s19017@gsm.hs.kr>
2020-08-18 18:12:59 +0900

    [ADD] add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import app from './app';
+import * as dotenv from 'dotenv';
 
-const port = 5000;
+//환경 변수 불러오기
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`)
