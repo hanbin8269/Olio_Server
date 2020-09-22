82e6897db5203c16bb10cbd9f32c581994d8faaf
hmoon630 <s19017@gsm.hs.kr>
2020-09-22 14:08:18 +0900

    [ADD] error handler

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,14 @@ import * as bodyParser from 'koa-bodyparser';
 import * as helmet from 'koa-helmet';
 
 import { router } from './routers';
+import { errorHandler } from './middlewares';
 
 const app = new Koa();
 
 app
   .use(helmet())
   .use(bodyParser())
+  .use(errorHandler())
   .use(router.routes())
   .use(router.allowedMethods());
 
