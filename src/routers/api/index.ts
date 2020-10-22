0448620f45a47fa0bc6667a0f200d26d7222bb2f
hmoon630 <s19017@gsm.hs.kr>
2020-10-22 17:11:15 +0900

    [ADD] need to login to access /project endpoint

--- src/routers/api/index.ts
+++ src/routers/api/index.ts
@@ -2,10 +2,11 @@ import * as Router from 'koa-router';
 
 import { user } from './user';
 import { project } from './project';
+import { loginRequired } from '../../middlewares/authorityChecker';
 
 const api = new Router();
 
 api.use('/user', user.routes());
-api.use('/project', project.routes());
+api.use('/project', loginRequired, project.routes());
 
 export { api };
