8842bbca29c54b3822313ac1e0597684a3a86438
hmoon630 <s19017@gsm.hs.kr>
2020-11-18 16:50:46 +0900

    [ADD] allow multipart data

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import * as Koa from 'koa';
 import * as bodyParser from 'koa-bodyparser';
 import * as helmet from 'koa-helmet';
+import * as koaBody from 'koa-body';
 
 import { sessionManager } from './middlewares';
 import { router } from './routers';
@@ -11,6 +12,7 @@ const app = new Koa();
 app
   .use(helmet())
   .use(bodyParser())
+  .use(koaBody({ multipart: true }))
   .use(errorHandler())
   .use(sessionManager)
   .use(router.routes())
