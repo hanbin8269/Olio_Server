03659aa6acf094a215c063bc7ee168492596730e
hmoon630 <s19017@gsm.hs.kr>
2020-11-21 18:52:02 +0900

    [ADD] S3 image module

--- src/libs/s3-image.ts
+++ src/libs/s3-image.ts
@@ -16,25 +16,57 @@ const s3 = new aws.S3({
   params: { Bucket: process.env.AWS_BUCKET_NAME },
 });
 
-export const uploadFile = async (
-  filePath: string,
-  fileName: string,
-  fileType: string
-) => {
-  const fileStream = createReadStream(filePath);
-  console.log(fileType);
-  const upload = s3.upload({
-    Bucket: process.env.AWS_BUCKET_NAME,
-    Key: fileName,
-    ACL: 'public-read',
-    Body: fileStream,
-    ContentType: 'image/png',
-  });
+const randomKey = () => {
+  return crypto.randomBytes(20).toString('hex');
+};
 
-  return await upload.send((err, data) => {
-    if (err) {
-      throw err;
-    }
-    return { key: data.Key, location: data.Location };
-  });
+export const uploadImage = async (filePath: string, fileType: string) => {
+  const fileStream = createReadStream(filePath);
+
+  let key = randomKey();
+  while (await getImage(key)) {
+    key = randomKey();
+  }
+
+  if (!fileType) {
+    fileType = 'png';
+  }
+
+  const upload: any = s3
+    .upload({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: key + '.' + fileType,
+      ACL: 'public-read',
+      Body: fileStream,
+      ContentType: fileType,
+    })
+    .promise();
+
+  return upload;
+};
+
+export const deleteImage = async (key: string) => {
+  const deletion: any = s3
+    .deleteObject({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: key,
+    })
+    .promise();
+
+  return deletion;
+};
+
+export const getImage = async (key: string) => {
+  try {
+    const file: any = s3
+      .getObject({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: key,
+      })
+      .promise();
+
+    return await file;
+  } catch {
+    return;
+  }
 };
